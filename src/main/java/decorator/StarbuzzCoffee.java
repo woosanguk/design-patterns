11d5a2eabeb50befa4a261644bedb53cb0cb3437
sanguk woo <baofree.uk@gmail.com>
2017-12-30 12:06:27 +0900

    decorator

---
+++ src/main/java/decorator/StarbuzzCoffee.java
@@ -0,0 +1,13 @@
+package decorator;
+
+class StarbuzzCoffee {
+    public static void main(String args[]){
+        Beverage beverage = new Espresso();
+        System.out.println(beverage.getDescription() + " $"+ beverage.cost());
+
+        beverage = new Mocha(beverage);
+
+        System.out.println(beverage.getDescription() + " $"+ beverage.cost());
+
+    }
+}
