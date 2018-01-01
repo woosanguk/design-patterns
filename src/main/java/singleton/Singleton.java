3f41aacb38655d46dd14fefee1a15c9fe8e93a85
sanguk woo <baofree.uk@gmail.com>
2018-01-01 09:12:42 +0900

    singleton

---
+++ src/main/java/singleton/Singleton.java
@@ -0,0 +1,14 @@
+package singleton;
+
+class Singleton {
+    private static Singleton uniqueInstance;
+
+    private Singleton(){}
+
+    public static Singleton getInstance(){
+        if (uniqueInstance == null){
+            uniqueInstance = new Singleton();
+        }
+        return uniqueInstance;
+    }
+}
