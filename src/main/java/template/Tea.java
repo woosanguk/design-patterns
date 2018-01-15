adf6e1b6f6619d0cb9194071a17a5aafb7956480
sanguk woo <baofree.uk@gmail.com>
2018-01-15 21:56:12 +0900

    template method

---
+++ src/main/java/template/Tea.java
@@ -0,0 +1,11 @@
+package template;
+
+class Tea extends CaffeineBeverage {
+    void brew() {
+        System.out.println("차를 우려내는 중");
+    }
+
+    void addCondiments() {
+        System.out.println("레몬을 추가하는 중");
+    }
+}
